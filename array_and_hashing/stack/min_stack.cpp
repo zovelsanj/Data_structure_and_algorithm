004a18a5ea08edbcd30a179a0ed0113be73ce9ce
zovelsanj <zovelsanj28@gmail.com>
2026-01-19 23:43:36 +0100

    added explanation w/example

--- array_and_hashing/stack/min_stack.cpp
+++ array_and_hashing/stack/min_stack.cpp
@@ -1,9 +1,7 @@
-/*150. Minimum Stack (Medium)
+/*Minimum Stack (Medium)
 Design a stack class that supports the push, pop, top, and getMin operations.
-MinStack() initializes the stack object.
-void push(int val) pushes the element val onto the stack.
-void pop() removes the element on the top of the stack.
-int top() gets the top element of the stack.
+MinStack() initializes the stack object. void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack. int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 Each function should run in O(1) time.
 */
