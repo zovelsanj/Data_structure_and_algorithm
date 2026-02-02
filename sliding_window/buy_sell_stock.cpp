96b506912d64dfbc4a1cf40294b126d940f01ef2
zovelsanj <zovelsanj28@gmail.com>
2026-02-02 01:40:07 +0100

    explanation added

--- sliding_window/buy_sell_stock.cpp
+++ sliding_window/buy_sell_stock.cpp
@@ -1,3 +1,16 @@
+/*Best Time to Buy and Sell Stock (Easy)
+
+You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+Example:
+Input: prices = [10,1,5,6,7,1]
+Output: 6
+
+You should aim for a solution with O(n) time and O(1) space, where n is the size of the input array.
+*/
+
 #include <iostream>
 #include <vector>
 
